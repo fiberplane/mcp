c4d3689f1c96e795ffe0cb7f37ffb60d182b8dae
Laurynas Keturakis <laurynas@fiberplane.com>
2025-09-18 14:04:08 +0300

    some renames and formatting

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,16 +7,16 @@ export {
 export { McpServer, type McpServerOptions } from "./core.js";
 export { RpcError } from "./errors.js";
 export {
-  createSSEStream,
-  type StreamWriter,
-} from "./sse-writer.js";
-export {
   type EventId,
+  InMemorySessionStore,
   type SessionId,
   type SessionMeta,
   type SessionStore,
-  InMemorySessionStore,
-} from "./store.js";
+} from "./session-store.js";
+export {
+  createSSEStream,
+  type StreamWriter,
+} from "./sse-writer.js";
 export {
   StreamableHttpTransport,
   type StreamableHttpTransportOptions,
